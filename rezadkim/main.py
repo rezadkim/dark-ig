e5c74807518dd8016950489f5cbb229ed854de05
Reza Adil Hakim <43247492+rezadkim@users.noreply.github.com>
2025-03-16 16:09:50 +0700

    Update main.py

--- rezadkim/main.py
+++ rezadkim/main.py
@@ -4,9 +4,9 @@ import subprocess
 import pkg_resources
 
 def main():
-    ihack = pkg_resources.resource_filename('rezadkim', 'dark')
-    if not os.access(ihack, os.X_OK): os.chmod(ihack, 0o755)
-    sys.exit(subprocess.call([ihack] + sys.argv[1:]))
+    darkige = pkg_resources.resource_filename('rezadkim', 'dark')
+    if not os.access(darkige, os.X_OK): os.chmod(darkige, 0o755)
+    sys.exit(subprocess.call([darkige] + sys.argv[1:]))
 
 if __name__ == '__main__':
     main()
