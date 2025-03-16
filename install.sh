7af8cbed3a38d5b01885b9276d3cf074cc37e92d
Reza Adil Hakim <43247492+rezadkim@users.noreply.github.com>
2025-03-16 18:34:16 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ folder="dark-ig"
 
 termux() {
     echo
-    echo -e "${W}[ ${B}INFO${W} ] ${G}dark-ig${W}: running installer for termux ..."
+    echo -e "${W}[ ${B}INFO${W} ] ${G}dark-ig${W}: running installer for Termux ..."
     echo -e "${W}[ ${B}INFO${W} ] ${G}dark-ig${W}: running update and upgrade package ..."
     echo
     pkg update -y
@@ -22,23 +22,23 @@ termux() {
     echo
     echo -e "${W}[ ${B}INFO${W} ] ${G}dark-ig${W}: installing ${Y}git${W}, ${Y}python3.10${W} ..."
     echo
-    pkg i -y git python3.10 binutils build-essential
+    pkg i -y git python3.10 binutils build-essential libffi libjpeg-turbo libcrypt
     echo
-    echo -e "${W}[ ${B}INFO${W} ] ${G}dark-ig${W}: cloning github repository ..."
+    echo -e "${W}[ ${B}INFO${W} ] ${G}dark-ig${W}: cloning GitHub repository ..."
     echo
     if [ -d $folder ]; then
         mv $folder "${folder}-backup"
     fi
     git clone --depth 1 https://github.com/rezadkim/dark-ig
     echo
-    echo -e "${W}[ ${B}INFO${W} ] ${G}dark-ig${W}: upgrading ${Y}pip3.10${W} ..."
+    echo -e "${W}[ ${B}INFO${W} ] ${G}dark-ig${W}: upgrading ${Y}pip${W} ..."
     echo
     python3.10 -m pip install --upgrade pip
     echo
-    echo -e "${W}[ ${B}INFO${W} ] ${G}dark-ig${W}: install ${Y}requirements.txt${W} with pip3.10 ..."
+    echo -e "${W}[ ${B}INFO${W} ] ${G}dark-ig${W}: install ${Y}requirements.txt${W} with pip ..."
     echo
     cd $folder || exit 1
-    python3.10 -m pip install -r requirements.txt
+    python3.10 -m pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org -r requirements.txt
     echo
     echo -e "${W}[ ${B}INFO${W} ] ${G}dark-ig${W}: install ${Y}darkig${W} with setuptools ..."
     echo
@@ -66,24 +66,24 @@ termux() {
 
 linux() {
     echo
-    echo -e "${W}[ ${B}INFO${W} ] ${G}dark-ig${W}: running installer for linux ..."
-    echo -e "${W}[ ${B}INFO${W} ] ${G}dark-ig${W}: adding package repository ${Y}ppa:savoury1/python${W} ..."
+    echo -e "${W}[ ${B}INFO${W} ] ${G}dark-ig${W}: running installer for Linux ..."
+    echo -e "${W}[ ${B}INFO${W} ] ${G}dark-ig${W}: adding package repository ${Y}ppa:deadsnakes/ppa${W} ..."
     echo
     sudo apt-get install software-properties-common -y
-    sudo add-apt-repository ppa:deadsnakes/ppa
-    sudo apt-get update
+    sudo add-apt-repository ppa:deadsnakes/ppa -y
+    sudo apt-get update -y
     echo
     echo -e "${W}[ ${B}INFO${W} ] ${G}dark-ig${W}: installing ${Y}git${W}, ${Y}python3.10${W} ..."
     echo
     sudo apt-get install git -y
     sudo apt-get install python3.10 python3.10-venv python3.10-distutils -y
     echo
-    echo -e "${W}[ ${B}INFO${W} ] ${G}dark-ig${W}: cloning github repository ..."
+    echo -e "${W}[ ${B}INFO${W} ] ${G}dark-ig${W}: cloning GitHub repository ..."
     echo
     if [ -d $folder ]; then
         mv $folder "${folder}-backup"
     fi
-    git clone --depth 1 https://github.com/iqbalmh18/dark-ig
+    git clone --depth 1 https://github.com/rezadkim/dark-ig
     echo
     echo -e "${W}[ ${B}INFO${W} ] ${G}dark-ig${W}: install ${Y}pip3.10${W} ..."
     echo
