ac38cb848dada3ff0a72c3d71dbd62ce00e8b489
Reza Adil Hakim <43247492+rezadkim@users.noreply.github.com>
2025-03-16 17:29:07 +0700

    Update main.py

--- rezadkim/main.py
+++ rezadkim/main.py
@@ -4,7 +4,7 @@ import subprocess
 import pkg_resources
 
 def main():
-    darkige = pkg_resources.resource_filename('rezadkim', 'dark')
+    darkige = pkg_resources.resource_filename('rezadkim', 'darkig')
     if not os.access(darkige, os.X_OK): os.chmod(darkige, 0o755)
     sys.exit(subprocess.call([darkige] + sys.argv[1:]))
 
